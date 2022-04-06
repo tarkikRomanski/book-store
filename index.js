d5a14c78be652ea73dd00b63a4fa1662560f08d8
Roman Prokopov <>
2022-04-06 19:56:22 +0300

    feat(all): added momento realisation

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const {
 const getLanguageMiddleware = require('./middlewares/get-language.middleware')
 const { sequelize } = require('./database')
 const { v4: uuid4 } = require('uuid');
+const {create} = require("./services/translate.service");
 
 const app = express()
 const port = 3000
@@ -22,7 +23,7 @@ app.use(getLanguageMiddleware)
 initUsersRoutes(app)
 initProductRoutes(app)
 
-app.post('/post', async (req, res) => {
+app.post('/posts', async (req, res) => {
     const {
         title,
         description
@@ -31,7 +32,7 @@ app.post('/post', async (req, res) => {
     const postId = uuid4()
 
     await sequelize.query(
-        'INSERT INTO posts (id, title, description) VALUES (:id, :title, :description)',
+        'INSERT INTO store.posts (id, title, description) VALUES (:id, :title, :description)',
         {
             replacements: {
                 id: postId,
@@ -42,7 +43,7 @@ app.post('/post', async (req, res) => {
     )
 
     await sequelize.query(
-        'INSERT INTO posts_snapshots (id, post_id, title, description) VALUES (:id, :postId, :title, :description)',
+        'INSERT INTO store.posts_snapshots (id, post_id, title, description) VALUES (:id, :postId, :title, :description)',
         {
             replacements: {
                 id: uuid4(),
@@ -53,16 +54,16 @@ app.post('/post', async (req, res) => {
         }
     )
 
-    res.json({
-        status: 'done'
-    })
+    res.redirect(`/posts/${postId}`)
 })
 
-app.put('/post/:id', async (req, res) => {
+app.post('/posts/:id', async (req, res) => {
     const {
         title,
         description
     } = req.body
+
+    console.log(req.body)
 
     const { id: postId } = req.params
 
@@ -78,7 +79,7 @@ app.put('/post/:id', async (req, res) => {
     )
 
     await sequelize.query(
-        'INSERT INTO posts_snapshots (id, post_id, title, description) VALUES (:id, :postId, :title, :description)',
+        'INSERT INTO store.posts_snapshots (id, post_id, title, description) VALUES (:id, :postId, :title, :description)',
         {
             replacements: {
                 id: uuid4(),
@@ -89,16 +90,14 @@ app.put('/post/:id', async (req, res) => {
         }
     )
 
-    res.json({
-        status: 'done'
-    })
+    res.redirect(`/posts/${postId}`)
 })
 
-app.put('/post/:id/revert', async (req, res) => {
+app.get('/posts/:id/revert', async (req, res) => {
     const { id: postId } = req.params
 
-    const results = sequelize.query(
-        'SELECT description, title FROM posts_snapshot WHERE post_id = :postId ORDER BY created_at desc LIMIT 2',
+    const [ results ] = await sequelize.query(
+        'SELECT id, description, title FROM store.posts_snapshots WHERE post_id = :postId ORDER BY created_at desc LIMIT 2',
         {
             replacements: {
                 postId
@@ -106,10 +105,16 @@ app.put('/post/:id/revert', async (req, res) => {
         }
     )
 
+    if (results.length < 2) {
+        res.redirect(`/posts/${postId}`)
+
+        return
+    }
+
     const [latestVersion, targetVersion] = results
 
     await sequelize.query(
-        'UPDATE posts SET description = :description, title = :title WHERE id = :id',
+        'UPDATE store.posts SET description = :description, title = :title WHERE id = :id',
         {
             replacements: {
                 id: postId,
@@ -120,7 +125,7 @@ app.put('/post/:id/revert', async (req, res) => {
     )
 
     await sequelize.query(
-        'DELETE FROM posts_snapshots WHERE id = :id',
+        'DELETE FROM store.posts_snapshots WHERE id = :id',
         {
             replacements: {
                 id: latestVersion.id
@@ -128,9 +133,94 @@ app.put('/post/:id/revert', async (req, res) => {
         }
     )
 
-    res.json({
-        status: 'done'
-    })
+    res.redirect(`/posts/${postId}`)
+})
+
+app.get('/posts/revert/:id', async (req, res) => {
+    const { id: snapshotId } = req.params
+
+    const [[ targetVersion ]] = await sequelize.query(
+        'SELECT * FROM store.posts_snapshots WHERE id = :snapshotId ORDER BY created_at desc LIMIT 1',
+        {
+            replacements: {
+                snapshotId
+            }
+        }
+    )
+
+    await sequelize.query(
+        'UPDATE store.posts SET description = :description, title = :title WHERE id = :id',
+        {
+            replacements: {
+                id: targetVersion.post_id,
+                title: targetVersion.title,
+                description: targetVersion.description
+            }
+        }
+    )
+
+    await sequelize.query(
+        'DELETE FROM store.posts_snapshots WHERE created_at > :created AND id != :id',
+        {
+            replacements: {
+                id: targetVersion.id,
+                created: targetVersion.created_at
+            }
+        }
+    )
+
+    res.redirect(`/posts/${targetVersion.post_id}`)
+})
+
+app.get('/posts', async (req, res) => {
+    const [posts] = await sequelize.query('SELECT * FROM store.posts')
+
+    console.log(posts)
+
+    res.render('posts/list', { posts })
+})
+
+app.get('/posts/create', async (req, res) => {
+    res.render('posts/create')
+})
+
+app.get('/posts/edit/:id', async (req, res) => {
+    const { id } = req.params
+
+    const [[ post ]] = await sequelize.query(
+        'SELECT * FROM store.posts WHERE id = :id LIMIT 1',
+        {
+            replacements: {
+                id
+            }
+        }
+    )
+
+    res.render('posts/edit', { post })
+})
+
+app.get('/posts/:id', async (req, res) => {
+    const { id } = req.params
+
+    const [[ post ]] = await sequelize.query(
+        'SELECT * FROM store.posts WHERE id = :id LIMIT 1',
+        {
+            replacements: {
+                id
+            }
+        }
+    )
+
+    const [ snapshots ] = await sequelize.query(
+        'SELECT * FROM store.posts_snapshots WHERE post_id = :id',
+        {
+            replacements: {
+                id
+            }
+        }
+    )
+
+    res.render('posts/item', { post, snapshots })
 })
 
 app.listen(port, () => {
